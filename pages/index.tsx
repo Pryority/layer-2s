e4493bb8d65e919dc9bb216fcf123989fd212253
pryority <matthewapryor@gmail.com>
2022-12-25 16:17:42 -0500

    add NFTs button to each layer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { ArrowTopRightOnSquareIcon, ArrowUpIcon, CubeTransparentIcon, ArrowUpOnSquareStackIcon, ArrowUpRightIcon, CircleStackIcon, MinusCircleIcon, PlusCircleIcon, ArrowDownIcon } from "@heroicons/react/24/outline";
+import { ArrowTopRightOnSquareIcon, ArrowUpIcon, CubeTransparentIcon, ArrowUpOnSquareStackIcon, ArrowUpRightIcon, CircleStackIcon, MinusCircleIcon, PlusCircleIcon, ArrowDownIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { motion } from "framer-motion";
 import Head from "next/head";
 import Link from "next/link";
@@ -123,11 +123,11 @@ export default function Home() {
           <h2 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[200px] text-red-900 uppercase tracking-tightest">
           Optimism
           </h2>
-          <section className="flex w-full items-center justify-center space-x-4">
-            <div className="p-1">
+          <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full items-center justify-around">
+            <div className="p-1 flex w-full justify-center">
               <Link
                 href={"https://kwenta.eth.limo"}
-                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex items-center space-x-2 lg:space-x-3 bg-red-700 border-red-300 text-red-50 transition-all ease-in-out duration-75 hover:bg-red-600 hover:border-red-200"
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit sm:w-full items-center space-x-2 lg:space-x-3 bg-red-700 border-red-300 text-red-50 transition-all ease-in-out duration-75 hover:bg-red-600 hover:border-red-200"
               >
                 <p className="text-xs md:text-base xl:text-xl">
                   Trade
@@ -135,15 +135,26 @@ export default function Home() {
                 <CircleStackIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
               </Link>
             </div>
-            <div className="p-1">
+            <div className="p-1 flex w-full justify-center">
               <Link
                 href={"https://app.optimism.io/bridge/"}
-                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex items-center space-x-2 lg:space-x-3 bg-red-700 border-red-300 text-red-50 transition-all ease-in-out duration-75 hover:bg-red-600 hover:border-red-200"
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit sm:w-full items-center space-x-2 lg:space-x-3 bg-red-700 border-red-300 text-red-50 transition-all ease-in-out duration-75 hover:bg-red-600 hover:border-red-200"
               >
                 <p className="text-xs md:text-base xl:text-xl">
                   Bridge
                 </p>
                 <CubeTransparentIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
+              </Link>
+            </div>
+            <div className="p-1 sm:col-span-2 md:col-span-1 flex w-full justify-center">
+              <Link
+                href={"https://app.optimism.io/bridge/"}
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit items-center space-x-2 lg:space-x-3 bg-red-700 border-red-300 text-red-50 transition-all ease-in-out duration-75 hover:bg-red-600 hover:border-red-200"
+              >
+                <p className="text-xs md:text-base xl:text-xl">
+                  NFTs
+                </p>
+                <SparklesIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
               </Link>
             </div>
           </section>
@@ -159,11 +170,11 @@ export default function Home() {
           <h2 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[200px] text-blue-900 uppercase tracking-tightest">
           Arbitrum
           </h2>
-          <section className="flex w-full items-center justify-center space-x-4">
-            <div className="p-1">
+          <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full items-center justify-around">
+            <div className="p-1 flex w-full justify-center">
               <Link
                 href={"https://app.1inch.io/#/1/unified/swap/ETH/DAI"}
-                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex items-center space-x-2 lg:space-x-3 bg-blue-700 border-blue-300 text-blue-50 transition-all ease-in-out duration-75 hover:bg-blue-600 hover:border-blue-200"
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit sm:w-full items-center space-x-2 lg:space-x-3 bg-blue-700 border-blue-300 text-blue-50 transition-all ease-in-out duration-75 hover:bg-blue-600 hover:border-blue-200"
               >
                 <p className="text-xs md:text-base xl:text-xl">
                   Trade
@@ -171,15 +182,26 @@ export default function Home() {
                 <CircleStackIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
               </Link>
             </div>
-            <div className="p-1">
+            <div className="p-1 flex w-full justify-center">
               <Link
                 href={"https://bridge.arbitrum.io/"}
-                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex items-center space-x-2 lg:space-x-3 bg-blue-700 border-blue-300 text-blue-50 transition-all ease-in-out duration-75 hover:bg-blue-600 hover:border-blue-200"
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit sm:w-full items-center space-x-2 lg:space-x-3 bg-blue-700 border-blue-300 text-blue-50 transition-all ease-in-out duration-75 hover:bg-blue-600 hover:border-blue-200"
               >
                 <p className="text-xs md:text-base xl:text-xl">
                   Bridge
                 </p>
                 <CubeTransparentIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
+              </Link>
+            </div>
+            <div className="p-1 sm:col-span-2 md:col-span-1 flex w-full justify-center">
+              <Link
+                href={"https://bridge.arbitrum.io/"}
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit items-center space-x-2 lg:space-x-3 bg-blue-700 border-blue-300 text-blue-50 transition-all ease-in-out duration-75 hover:bg-blue-600 hover:border-blue-200"
+              >
+                <p className="text-xs md:text-base xl:text-xl">
+                  NFTs
+                </p>
+                <SparklesIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
               </Link>
             </div>
           </section>
@@ -195,11 +217,11 @@ export default function Home() {
           <h2 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[200px] text-purple-900 uppercase tracking-tightest">
           Starknet
           </h2>
-          <section className="flex w-full items-center justify-center space-x-4">
-            <div className="p-1">
+          <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full items-center justify-around">
+            <div className="p-1 flex w-full justify-center">
               <Link
                 href={"https://www.starkswap.co/app/swap"}
-                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex items-center space-x-2 lg:space-x-3 bg-purple-700 border-purple-300 text-purple-50 transition-all ease-in-out duration-75 hover:bg-purple-600 hover:border-purple-200"
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit sm:w-full items-center space-x-2 lg:space-x-3 bg-purple-700 border-purple-300 text-purple-50 transition-all ease-in-out duration-75 hover:bg-purple-600 hover:border-purple-200"
               >
                 <p className="text-xs md:text-base xl:text-xl">
                   Trade
@@ -207,15 +229,26 @@ export default function Home() {
                 <CircleStackIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
               </Link>
             </div>
-            <div className="p-1">
+            <div className="p-1 flex w-full justify-center">
               <Link
                 href={"https://starkgate.starknet.io/"}
-                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex items-center space-x-2 lg:space-x-3 bg-purple-700 border-purple-300 text-purple-50 transition-all ease-in-out duration-75 hover:bg-purple-600 hover:border-purple-200"
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit sm:w-full items-center space-x-2 lg:space-x-3 bg-purple-700 border-purple-300 text-purple-50 transition-all ease-in-out duration-75 hover:bg-purple-600 hover:border-purple-200"
               >
                 <p className="text-xs md:text-base xl:text-xl">
                   Bridge
                 </p>
                 <CubeTransparentIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
+              </Link>
+            </div>
+            <div className="p-1 sm:col-span-2 md:col-span-1 flex w-full justify-center">
+              <Link
+                href={"https://starkgate.starknet.io/"}
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit items-center space-x-2 lg:space-x-3 bg-purple-700 border-purple-300 text-purple-50 transition-all ease-in-out duration-75 hover:bg-purple-600 hover:border-purple-200"
+              >
+                <p className="text-xs md:text-base xl:text-xl">
+                  NFTs
+                </p>
+                <SparklesIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
               </Link>
             </div>
           </section>
@@ -231,11 +264,11 @@ export default function Home() {
           <h2 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[200px] text-violet-900 uppercase tracking-tightest">
           zkSync
           </h2>
-          <section className="flex w-full items-center justify-center space-x-4">
-            <div className="p-1">
+          <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full items-center justify-around">
+            <div className="p-1 flex w-full justify-center">
               <Link
                 href={"https://trade.zigzag.exchange/"}
-                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex items-center space-x-2 lg:space-x-3 bg-violet-700 border-violet-300 text-violet-50 transition-all ease-in-out duration-75 hover:bg-violet-600 hover:border-violet-200"
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit sm:w-full items-center space-x-2 lg:space-x-3 bg-violet-700 border-violet-300 text-violet-50 transition-all ease-in-out duration-75 hover:bg-violet-600 hover:border-violet-200"
               >
                 <p className="text-xs md:text-base xl:text-xl">
                   Trade
@@ -243,15 +276,26 @@ export default function Home() {
                 <CircleStackIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
               </Link>
             </div>
-            <div className="p-1">
+            <div className="p-1 flex w-full justify-center">
               <Link
                 href={"https://portal.zksync.io/bridge"}
-                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex items-center space-x-2 lg:space-x-3 bg-violet-700 border-violet-300 text-violet-50 transition-all ease-in-out duration-75 hover:bg-violet-600 hover:border-violet-200"
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit sm:w-full items-center space-x-2 lg:space-x-3 bg-violet-700 border-violet-300 text-violet-50 transition-all ease-in-out duration-75 hover:bg-violet-600 hover:border-violet-200"
               >
                 <p className="text-xs md:text-base xl:text-xl">
                   Bridge
                 </p>
                 <CubeTransparentIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
+              </Link>
+            </div>
+            <div className="p-1 sm:col-span-2 md:col-span-1 flex w-full justify-center">
+              <Link
+                href={"https://portal.zksync.io/bridge"}
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit items-center space-x-2 lg:space-x-3 bg-violet-700 border-violet-300 text-violet-50 transition-all ease-in-out duration-75 hover:bg-violet-600 hover:border-violet-200"
+              >
+                <p className="text-xs md:text-base xl:text-xl">
+                  NFTs
+                </p>
+                <SparklesIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
               </Link>
             </div>
           </section>
@@ -267,11 +311,11 @@ export default function Home() {
           <h2 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[200px] text-lime-900 uppercase tracking-tightest">
           Boba
           </h2>
-          <section className="flex w-full items-center justify-center space-x-4">
-            <div className="p-1">
+          <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full items-center justify-around">
+            <div className="p-1 flex w-full justify-center">
               <Link
                 href={"https://oolongswap.com/#/"}
-                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex items-center space-x-2 lg:space-x-3 bg-lime-700 border-lime-300 text-lime-50 transition-all ease-in-out duration-75 hover:bg-lime-600 hover:border-lime-200"
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit sm:w-full items-center space-x-2 lg:space-x-3 bg-lime-700 border-lime-300 text-lime-50 transition-all ease-in-out duration-75 hover:bg-lime-600 hover:border-lime-200"
               >
                 <p className="text-xs md:text-base xl:text-xl">
                   Trade
@@ -279,15 +323,26 @@ export default function Home() {
                 <CircleStackIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
               </Link>
             </div>
-            <div className="p-1">
+            <div className="p-1 flex w-full justify-center">
               <Link
                 href={"https://gateway.boba.network/"}
-                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex items-center space-x-2 lg:space-x-3 bg-lime-700 border-lime-300 text-lime-50 transition-all ease-in-out duration-75 hover:bg-lime-600 hover:border-lime-200"
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit sm:w-full items-center space-x-2 lg:space-x-3 bg-lime-700 border-lime-300 text-lime-50 transition-all ease-in-out duration-75 hover:bg-lime-600 hover:border-lime-200"
               >
                 <p className="text-xs md:text-base xl:text-xl">
                   Bridge
                 </p>
                 <CubeTransparentIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
+              </Link>
+            </div>
+            <div className="p-1 sm:col-span-2 md:col-span-1 flex w-full justify-center">
+              <Link
+                href={"https://gateway.boba.network/"}
+                className="px-6 py-2 border-[3px] lg:border-4 rounded-full flex justify-center w-fit items-center space-x-2 lg:space-x-3 bg-lime-700 border-lime-300 text-lime-50 transition-all ease-in-out duration-75 hover:bg-lime-600 hover:border-lime-200"
+              >
+                <p className="text-xs md:text-base xl:text-xl">
+                  NFTs
+                </p>
+                <SparklesIcon className="h-4 w-4 md:h-5 md:w-5 xl:h-6 xl:w-6"/>
               </Link>
             </div>
           </section>
